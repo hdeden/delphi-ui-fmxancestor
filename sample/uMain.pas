6b415c53dca21ab3788c7e83b0a9d628995f862b
unknown <deden@kreativia.digital>
2020-11-13 21:34:51 +0700

    Change Icon, Scrool Vieport, add sample form

---
+++ sample/uMain.pas
@@ -0,0 +1,26 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  ua04LayoutSTMFooter, System.ImageList, FMX.ImgList, FMX.MultiView,
+  FMX.Objects, FMX.Layouts, FMX.Controls.Presentation;
+
+type
+  Tfra04LayoutSTMFooter4 = class(Tfra04LayoutSTMFooter)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fra04LayoutSTMFooter4: Tfra04LayoutSTMFooter4;
+
+implementation
+
+{$R *.fmx}
+
+end.
